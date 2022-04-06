ae4e949ed13b9de5494ac676e6c6628d47fbc22f
Valdenício Ferreira <valdenicio22@gmail.com>
2022-04-06 01:23:15 -0300

    Add forgotPassword page components

--- src/containers/ForgotPassword/index.tsx
+++ src/containers/ForgotPassword/index.tsx
@@ -1,9 +1,71 @@
+import Arrow from 'components/Arrow'
+import { TextField } from 'components/TextField'
 import { GetServerSideProps } from 'next'
 import { withSSRGuest } from 'utils/withSSRGuest'
 import * as S from './ForgotPassword'
+import { useForm, SubmitHandler } from 'react-hook-form'
+import { api } from 'service/api'
+import Button from 'components/Button'
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+type ForgotPasswordData = {
+  email: string
+}
 
 export default function ForgotPassword() {
-  return <S.Wrapper>Esqueceu a senha Page</S.Wrapper>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<ForgotPasswordData>()
+
+  const onSubmit: SubmitHandler<ForgotPasswordData> = async (formData) => {
+    try {
+      const response = await api.patch<ForgotPasswordData>(
+        '/users/resetpassword',
+        { formData }
+      )
+      console.log(response)
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (err: any) {
+      if (err.response.status === 404) {
+        toast.error('Usuário não encontrado')
+      }
+    }
+  }
+  return (
+    <S.Wrapper>
+      <S.ArrowContainer>
+        <Arrow />
+      </S.ArrowContainer>
+      <S.InfoContainer>
+        <S.H1>Esqueci minha senha</S.H1>
+        <p>
+          Enviaremos uma senha para o e-mail cadastra-do, para que você possa
+          redefinir sua senha.
+        </p>
+      </S.InfoContainer>
+      <S.Form onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          label="Seu e-mail:*"
+          type="email"
+          {...register('email', {
+            required: true,
+            pattern: /\S+@\S+\.\S+/
+          })}
+          placeholder="lumasilva@email.com"
+        />
+        <S.ErrorMessageContainer>
+          {errors.email?.type === 'pattern' && 'Email inválido'}
+          {errors.email?.type === 'required' && 'Email é um campo obrigatório'}
+        </S.ErrorMessageContainer>
+        <S.BtnContainer>
+          <Button>enviar para email</Button>
+        </S.BtnContainer>
+      </S.Form>
+    </S.Wrapper>
+  )
 }
 
 export const getServerSideProps: GetServerSideProps = withSSRGuest(async () => {
