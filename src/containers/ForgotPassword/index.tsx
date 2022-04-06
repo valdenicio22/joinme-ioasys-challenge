1bc89c3995c4fda6361e53d3ebf45053b6c577cb
Valdenício Ferreira <valdenicio22@gmail.com>
2022-04-05 23:46:28 -0300

    Add redirect to forgetPassowrd page

--- src/containers/ForgotPassword/index.tsx
+++ src/containers/ForgotPassword/index.tsx
@@ -1,5 +1,13 @@
+import { GetServerSideProps } from 'next'
+import { withSSRGuest } from 'utils/withSSRGuest'
 import * as S from './ForgotPassword'
 
 export default function ForgotPassword() {
-  return <S.Wrapper></S.Wrapper>
+  return <S.Wrapper>Esqueceu a senha Page</S.Wrapper>
 }
+
+export const getServerSideProps: GetServerSideProps = withSSRGuest(async () => {
+  return {
+    props: {}
+  }
+})
