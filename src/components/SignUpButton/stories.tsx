856c0acadcdd936114854e873a2e8d80acac804d
Valdenício Ferreira <valdenicio22@gmail.com>
2022-04-06 21:53:00 -0300

    refact: remove signUpButton component

--- src/components/SignUpButton/stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story, Meta } from '@storybook/react'
-import SignUpButton from '.'
-
-export default {
-  title: 'SignUpButton',
-  component: SignUpButton
-} as Meta
-
-export const Default: Story = () => <SignUpButton />
