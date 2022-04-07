aa1bec0edac35d5e8e9537b96bc43e315544b6dc
Valdenício Ferreira <valdenicio22@gmail.com>
2022-04-06 22:10:25 -0300

    feat: add errorTextField storie

--- src/components/TextField/stories.tsx
+++ src/components/TextField/stories.tsx
@@ -1,9 +1,32 @@
 import { Story, Meta } from '@storybook/react'
-import { TextField } from '.'
+import EyeIcon from 'components/EyeIcon'
+
+import { TextField, TextFieldProps } from '.'
 
 export default {
   title: 'TextField',
-  component: TextField
+  component: TextField,
+  args: {
+    label: 'E-mail',
+    labelFor: 'Email',
+    id: 'Email',
+    icons: <EyeIcon />,
+    placeholder: 'lumaCamp@email.com'
+  }
 } as Meta
 
-export const Default: Story = () => <TextField />
+export const Default: Story<TextFieldProps> = (args) => (
+  <div style={{ maxWidth: 300, padding: 15 }}>
+    <TextField {...args} />
+  </div>
+)
+
+export const withError: Story<TextFieldProps> = (args) => (
+  <div style={{ maxWidth: 300, padding: 15 }}>
+    <TextField {...args} />
+  </div>
+)
+
+withError.args = {
+  error: 'Ops...something is wrong'
+}
