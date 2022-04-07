0fdbd110c835f250c075fd416cb6ae7eef6e47d4
Valdenício Ferreira <valdenicio22@gmail.com>
2022-04-07 01:19:32 -0300

    Create MagnifierIcon component

---
+++ src/components/MagnifierIcon/test.tsx
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react'
+
+import MagnifierIcon from '.'
+
+describe('<MagnifierIcon />', () => {
+  it('should render the magnifierIcon', () => {
+    render(<MagnifierIcon />)
+  })
+})
