87653d0b3535b2ef6bc92c9aa9216b7195370bc2
Valdenício Ferreira <valdenicio22@gmail.com>
2022-04-10 13:49:10 -0300

    style: add new header style

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,37 +1,17 @@
-import Button from 'components/Button'
-import Logo from 'components/Logo'
-import { useAuth, signOut } from 'context/AuthContext'
-import Link from 'next/link'
+import Logo from '../Logo'
 import Router from 'next/router'
 import * as S from './styles'
 
 const Header = () => {
-  const { user, setUser } = useAuth()
-
-  const handleLogout = () => {
-    setUser(undefined!)
-    signOut()
-  }
-
   return (
     <S.Wrapper>
-      <S.ContentWrapper>
-        <Logo />
-        <S.NavContainer>
-          <Link href="/">Home</Link>
-          <Link href="/">Posts</Link>
-        </S.NavContainer>
-        {user ? (
-          <>
-            <p>
-              Bem vindo: <span>{user.name}</span>
-            </p>
-            <Button onClick={handleLogout}>Logout</Button>
-          </>
-        ) : (
-          <Button onClick={() => Router.push('/login')}>Login</Button>
-        )}
-      </S.ContentWrapper>
+      <S.LogoBtnContainer onClick={() => Router.push('/')}>
+        <Logo color="black" />
+      </S.LogoBtnContainer>
+      <S.NavContainer>
+        <S.NavButton>Entrar</S.NavButton>
+        <S.NavButton>Cadastra-se</S.NavButton>
+      </S.NavContainer>
     </S.Wrapper>
   )
 }
