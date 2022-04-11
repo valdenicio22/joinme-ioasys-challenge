7bd02ab5d82462b375360b66f61c94264d082f49
Valdenício Ferreira <valdenicio22@gmail.com>
2022-04-11 11:48:36 -0300

    feat: change the logic of emergencyContct and interests modal to components

--- src/containers/Dashboard/Dashboard.styles.ts
+++ src/containers/Dashboard/Dashboard.styles.ts
@@ -1,12 +1,6 @@
-import styled, { css, DefaultTheme } from 'styled-components'
-import { Activity } from '.'
-
-type InterestProps = Pick<Activity, 'isSelect'>
+import styled, { css } from 'styled-components'
 
 export const ModalContainer = styled.div`
-  width: 43rem;
-  margin: 0 auto;
-
   display: flex;
   align-items: center;
   justify-content: center;
@@ -41,12 +35,6 @@ export const H2 = styled.h2`
   margin-left: 1rem;
 `
 
-export const ArrowContainer = styled.div`
-  margin-left: 1rem;
-  margin-bottom: 2.5rem;
-  cursor: pointer;
-`
-
 export const InputsContainer = styled.div`
   width: 100%;
   display: flex;
@@ -55,41 +43,6 @@ export const InputsContainer = styled.div`
   flex-direction: column;
   gap: 1.5rem;
   margin-bottom: 4rem;
-`
-
-export const InterestsContainer = styled.div`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  flex-flow: row wrap;
-  gap: 2rem;
-  margin-top: 3rem;
-  cursor: pointer;
-`
-
-const IconContentModifiers = {
-  isSelect: (theme: DefaultTheme) => css`
-    border: 2px solid ${theme.colors.primary};
-  `
-}
-
-export const InterestContent = styled.main`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  flex-direction: column;
-`
-
-export const InterestIcon = styled.div<InterestProps>`
-  ${({ theme, isSelect }) => css`
-    background-color: #ccc;
-    width: 8rem;
-    height: 8rem;
-    border-radius: 50%;
-    margin-bottom: 1.2rem;
-    ${isSelect && IconContentModifiers.isSelect(theme)}
-  `}
 `
 
 export const Interest = styled.div`
@@ -106,12 +59,6 @@ export const InterestFooter = styled.footer`
   justify-content: center;
 `
 
-export const SkipStep = styled.span`
-  cursor: pointer;
-  ${({ theme }) => css`
-    color: ${theme.colors.primary};
-  `}
-`
 export const Wrapper = styled.div`
   width: 76.8rem;
   margin: 0 auto;
@@ -119,14 +66,6 @@ export const Wrapper = styled.div`
   border: 2px solid black;
 `
 
-export const ButtonsContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  gap: 1rem;
-  margin-top: 3rem;
-`
 export const HeaderContainer = styled.header`
   width: 100%;
   display: flex;
--- src/containers/Dashboard/index.tsx
+++ src/containers/Dashboard/index.tsx
@@ -1,40 +1,21 @@
-import Button from '../../components/Button'
-import { TextField } from '../../components/TextField'
-import Fakelogo from '../../components/Fakelogo'
-import { Dialog } from '../../components/Dialog'
-import Arrow from '../../components/Arrow'
-
-import Head from 'next/head'
+import { useState } from 'react'
 import { GetServerSideProps } from 'next'
+import Head from 'next/head'
+
+import Profile from 'components/Profile'
+import { Dialog } from '../../components/Dialog'
+import { Interests } from '../../components/Interests'
+import { EmergencyContact } from '../../components/EmergencyContact'
 
 import { withSSRAuth } from '../../utils/withSSRAuth'
-import { useAuth } from '../../context/AuthContext'
-import { ActivityData, User } from '../../types/types'
 
 import * as S from './Dashboard.styles'
 
-import { useForm, SubmitHandler } from 'react-hook-form'
-import { api } from '../../service/api'
-
-import { useEffect, useState } from 'react'
-import { setCookie } from 'nookies'
-
-import { toast } from 'react-toastify'
 import Drawer from 'react-modern-drawer'
-import Profile from 'components/Profile'
-
-type SecurityContactData = Pick<User, 'emergencyName' | 'emergencyPhone'>
-
-export type Activity = {
-  name: string
-  id: string
-  isSelect: boolean
-}
 
 export default function Dashboard() {
-  const { user, setUser } = useAuth()
   const [modalStep, setModalStep] = useState(1)
-  const [activities, setActivities] = useState<Activity[]>([])
+
   const [isDrawerOpen, setIsDrawerOpen] = useState(false)
 
   const toggleDrawer = () => {
@@ -44,72 +25,6 @@ export default function Dashboard() {
   const [isModalOpen, setIsModalOpen] = useState(true)
 
   const onCloseModal = () => setIsModalOpen(false)
-
-  const { register, handleSubmit } = useForm<SecurityContactData>()
-
-  const onSubmit: SubmitHandler<SecurityContactData> = async (formData) => {
-    if (formData) {
-      const updatedUser = {
-        ...user!,
-        emergencyName: formData.emergencyName,
-        emergencyPhone: formData.emergencyPhone
-      }
-      try {
-        console.log({ updatedUser })
-        const response = await api.patch<{ updatedUser: User }>('/users', {
-          updatedUser
-        })
-        setUser(response.data.updatedUser)
-        setCookie(undefined, 'joinMeUser', JSON.stringify(updatedUser), {
-          maxAge: 60 * 60 * 24 * 30, //30 days
-          path: '/'
-        })
-        setModalStep(2)
-      } catch (error) {
-        console.log(error)
-      }
-    }
-  }
-
-  const handleSkipModalStep = () =>
-    modalStep > 1 ? onCloseModal() : setModalStep(2)
-
-  useEffect(() => {
-    api
-      .get<Array<ActivityData>>('/activities/list')
-      .then((response) => {
-        const activityData = response.data
-        const updatedActivities = activityData.map((activity) => ({
-          ...activity,
-          isSelect: false
-        }))
-        setActivities(updatedActivities)
-      })
-      .catch((error) => console.log(error))
-  }, [])
-
-  const handleActivitySelected = (selectedActivity: Activity) => {
-    const activitiesUpdated = activities.map((activity) =>
-      activity.id === selectedActivity.id
-        ? { ...activity, isSelect: !selectedActivity.isSelect }
-        : activity
-    )
-    setActivities(activitiesUpdated)
-  }
-
-  const handleUserInterests = async () => {
-    const activityIds = activities.reduce((acc, activity) => {
-      return activity.isSelect === true ? [...acc, activity.id] : acc
-    }, [] as Array<string>)
-    try {
-      await api.post('/users/interests', { activityIds })
-      toast.success('Interesses cadastrado com sucesso')
-    } catch (error) {
-      console.log(error)
-      toast.error('error')
-    }
-    onCloseModal()
-  }
 
   return (
     <S.Wrapper>
@@ -125,65 +40,18 @@ export default function Dashboard() {
       >
         <Profile />
       </Drawer>
+
       <Dialog isModalOpen={isModalOpen} onCloseModal={onCloseModal}>
-        {modalStep === 1 && (
-          <>
-            <S.LogoContainer>
-              <Fakelogo />
-            </S.LogoContainer>
-            <S.FormContainer onSubmit={handleSubmit(onSubmit)}>
-              <S.TitleContainer>
-                <S.H2>
-                  Gostaria de adiconar <br /> um contato de segurança ?
-                </S.H2>
-              </S.TitleContainer>
-              <S.InputsContainer>
-                <TextField
-                  label="Qual o nome da pessoa?"
-                  type="text"
-                  {...register('emergencyName')}
-                  placeholder="luma silva"
-                  fullWidth={true}
-                />
-                <TextField
-                  label="E o telefone?"
-                  type="text"
-                  placeholder="(xx) 9 9999-9999"
-                  fullWidth={true}
-                  {...register('emergencyPhone')}
-                />
-              </S.InputsContainer>
-              <S.ButtonsContainer>
-                <S.SkipStep onClick={handleSkipModalStep}>pular</S.SkipStep>
-                <Button type="submit">proximo</Button>
-              </S.ButtonsContainer>
-            </S.FormContainer>
-          </>
-        )}
+        {modalStep === 1 && <EmergencyContact setModalStep={setModalStep} />}
         {modalStep === 2 && (
-          <>
-            <S.ArrowContainer onClick={() => setModalStep(1)}>
-              <Arrow />
-            </S.ArrowContainer>
-            <S.H2>Conta pra gente...quais são seus interesses</S.H2>
-            <S.InterestsContainer>
-              {activities.map((activity) => (
-                <S.InterestContent
-                  key={activity.id}
-                  onClick={() => handleActivitySelected(activity)}
-                >
-                  <S.InterestIcon isSelect={activity.isSelect}></S.InterestIcon>
-                  <p>{activity.name}</p>
-                </S.InterestContent>
-              ))}
-            </S.InterestsContainer>
-            <S.ButtonsContainer>
-              <S.SkipStep onClick={handleSkipModalStep}>pular</S.SkipStep>
-              <Button onClick={handleUserInterests}>Cadastrar</Button>
-            </S.ButtonsContainer>
-          </>
+          <Interests
+            setModalStep={setModalStep}
+            onCloseModal={onCloseModal}
+            modalStep={modalStep}
+          />
         )}
       </Dialog>
+
       <S.HeaderContainer>
         <S.WelcomeContainer>
           <S.ProfilePicture />
