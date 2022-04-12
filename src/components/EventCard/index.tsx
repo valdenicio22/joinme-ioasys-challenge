e187ff917ea134f25b78b66b713809960694a369
Valdenício Ferreira <valdenicio22@gmail.com>
2022-04-12 00:50:16 -0300

    feat: create EventCard component and styled it

--- src/components/EventCard/index.tsx
+++ src/components/EventCard/index.tsx
@@ -1,18 +1,55 @@
+import BookMark from 'components/BookMark'
+import LocationIcon from 'components/LocationIcon'
 import Image from 'next/image'
+import { useState } from 'react'
+import { EventData } from 'types/types'
 import * as S from './styles'
 
-export const EventCard = () => {
+type EventCardProps = {
+  date: EventData['date']
+  name: EventData['name']
+  city: EventData['city']
+  description?: EventData['description']
+  activity_id?: EventData['activity_id']
+  max_participants: EventData['max_participants']
+}
+
+export const EventCard = ({
+  city,
+  date,
+  max_participants,
+  name,
+  description
+}: EventCardProps) => {
+  const [isBooked, setIsBooked] = useState(false)
+
   return (
     <S.Wrapper>
       <S.ImgContainer>
         <Image width={160} height={100} src="/test.webp" alt="Teste" />
       </S.ImgContainer>
       <S.EventDetails>
-        <S.Schedule>13 de Abr. De 2022 20:00 BRT</S.Schedule>
+        <S.Schedule>{date}</S.Schedule>
+        <S.EventTitle>{name}</S.EventTitle>
 
-        <S.EventTitle>Lets Make Friends!</S.EventTitle>
-        <S.Participants>45 participantes</S.Participants>
-        <S.Flags>Icon to favorite</S.Flags>
+        <S.Description>{description}</S.Description>
+
+        <S.ExtraInfo>
+          <S.Participants>
+            {max_participants > 1
+              ? `${max_participants} participantes`
+              : `${max_participants} participante`}
+          </S.Participants>
+          <S.Location>
+            <LocationIcon />
+            <S.City>{city}</S.City>
+          </S.Location>
+        </S.ExtraInfo>
+        <S.Flags>
+          <S.IconButton onClick={() => setIsBooked(!isBooked)}>
+            <BookMark isBooked={isBooked} />
+          </S.IconButton>
+        </S.Flags>
       </S.EventDetails>
     </S.Wrapper>
   )
--- src/components/EventCard/styles.ts
+++ src/components/EventCard/styles.ts
@@ -4,7 +4,8 @@ export const Wrapper = styled.div`
   width: 100%;
   height: 16rem;
   border-top: 1px solid ${({ theme }) => theme.colors.lightGray};
-  padding: 1.8rem 0;
+  padding-top: 1.5rem;
+  padding-bottom: 2rem;
 
   display: flex;
   align-items: center;
@@ -12,9 +13,8 @@ export const Wrapper = styled.div`
 `
 
 export const ImgContainer = styled.div`
-  width: 16rem;
-  height: 12.5rem;
-  margin-top: 2.5rem;
+  width: 20rem;
+  height: 100%;
   img {
     border-radius: 1rem;
   }
@@ -22,14 +22,18 @@ export const ImgContainer = styled.div`
 
 export const EventDetails = styled.div`
   width: 100%;
-  height: 12.5rem;
+  height: 100%;
   display: flex;
   align-items: flex-start;
-  justify-content: center;
+  justify-content: flex-start;
   flex-direction: column;
 `
 
-export const Schedule = styled.div``
+export const Schedule = styled.p`
+  margin-bottom: 1rem;
+`
+
+export const Description = styled.p``
 
 export const EventTitle = styled.h2`
   ${({ theme }) => css`
@@ -39,8 +43,32 @@ export const EventTitle = styled.h2`
   `}
 `
 
-export const Participants = styled.div``
+export const ExtraInfo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1.5rem;
+  margin-top: 2rem;
+`
+
+export const Participants = styled.span``
+
+export const Location = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.3rem;
+`
+
+export const City = styled.p``
 
 export const Flags = styled.div`
-  margin-top: 1rem;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`
+
+export const IconButton = styled.button`
+  background: transparent;
+  border: none;
+  outline: none;
 `
