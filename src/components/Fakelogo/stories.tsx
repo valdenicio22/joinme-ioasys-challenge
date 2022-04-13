fe30950f5241cf4f605fdf947a45e20dafddb6bc
Valdenício Ferreira <valdenicio22@gmail.com>
2022-04-12 23:17:42 -0300

    feat: remove fakeLogo component

--- src/components/Fakelogo/stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story, Meta } from '@storybook/react'
-import Fakelogo from '.'
-
-export default {
-  title: 'Fakelogo',
-  component: Fakelogo
-} as Meta
-
-export const Default: Story = (args) => <Fakelogo {...args} />
