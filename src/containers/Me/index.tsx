040bb73b78f738d2a8cc3c984d83015f374c7615
Valdenício Ferreira <valdenicio22@gmail.com>
2022-04-13 07:36:03 -0300

    feat: add first elements and basic style to the editUserPage

--- src/containers/Me/index.tsx
+++ src/containers/Me/index.tsx
@@ -1,8 +1,43 @@
 import { GetServerSideProps } from 'next'
+import Router from 'next/router'
 import { withSSRAuth } from 'utils/withSSRAuth'
+import { useAuth } from 'context/AuthContext'
+
+import Arrow from 'components/Arrow'
+import Logo from 'components/Logo'
+
+import * as S from './styles'
 
 export default function Me() {
-  return <div>Editando Minha Página de perfil</div>
+  const { user } = useAuth()
+
+  return (
+    <S.Wrapper>
+      <S.Header>
+        <S.ArrowContainer onClick={() => Router.push('/dashboard')}>
+          <Arrow />
+        </S.ArrowContainer>
+        <S.Page>Perfil</S.Page>
+      </S.Header>
+
+      <S.LogoAndName>
+        <Logo />
+        <S.PersonName>{user?.name}</S.PersonName>
+      </S.LogoAndName>
+
+      <S.AccountInfo>
+        <S.SmallInfoCard>
+          2 <span>Eventos</span>
+        </S.SmallInfoCard>
+        <S.SmallInfoCard>
+          2 <span>Badge</span>
+        </S.SmallInfoCard>
+        <S.SmallInfoCard>
+          140 <span>minutos</span>
+        </S.SmallInfoCard>
+      </S.AccountInfo>
+    </S.Wrapper>
+  )
 }
 
 export const getServerSideProps: GetServerSideProps = withSSRAuth(async () => {
--- src/containers/Me/styles.ts
+++ src/containers/Me/styles.ts
@@ -1,3 +1,98 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-export const Wrapper = styled.div``
+export const Wrapper = styled.div`
+  ${({ theme }) => css`
+    width: ${theme.containers.mobile};
+    margin: 2rem auto;
+  `}
+  border: 1px solid black;
+`
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+export const ArrowContainer = styled.button`
+  background: transparent;
+  outline: none;
+  border: none;
+
+  color: ${({ theme }) => theme.colors.primary};
+`
+
+export const Page = styled.h2`
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.medium};
+  `}
+`
+export const LogoAndName = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`
+
+export const PersonName = styled.p`
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.large};
+    line-height: 3rem;
+  `}
+`
+
+export const AccountInfo = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, minmax(10rem, 1fr));
+  gap: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export const SmallInfoCard = styled.div`
+  width: 10rem;
+  height: 8rem;
+  background-color: #dedaf0;
+  color: #4c33fd;
+  font-weight: ${({ theme }) => theme.font.weight.bold};
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 1.6rem;
+  flex-direction: column;
+
+  span {
+    color: #757678;
+    font-weight: ${({ theme }) => theme.font.weight.regular};
+  }
+`
+
+export const Level = styled.div`
+  width: 100%;
+  height: 8rem;
+  background-color: '#ccc';
+  border: 1px solid black;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 1.6rem;
+  flex-direction: column;
+  background-color: ${({ theme }) => theme.colors.lighterBlue};
+`
+
+export const Minuts = styled.div`
+  width: 100%;
+  height: 8rem;
+  background-color: '#ccc';
+  border: 1px solid black;
+  border-radius: 1.6rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  background-color: ${({ theme }) => theme.colors.lighterBlue};
+`
