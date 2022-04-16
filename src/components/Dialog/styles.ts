3021372bbe03c72fb1900181fb60539cdc52881b
Valdenício Ferreira <valdenicio22@gmail.com>
2022-04-16 03:48:47 -0300

    style: fix size of Daialog Modal component

--- src/components/Dialog/styles.ts
+++ src/components/Dialog/styles.ts
@@ -3,11 +3,10 @@ import Modal from 'react-modal'
 
 export const StyledModal = styled(Modal)`
   &.react-modal-content {
-    width: 100%;
-    max-width: 43rem;
+    width: 43rem;
     background-color: #f0f2f5;
     padding: 6rem 2.5rem;
     position: relative;
-    border-radius: 3rem;
+    border-radius: 2.2rem;
   }
 `
