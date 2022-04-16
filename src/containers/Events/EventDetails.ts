d924c5e1e61f0b0ca85bd720537b6a9de9db3889
Valdenício Ferreira <valdenicio22@gmail.com>
2022-04-16 17:50:31 -0300

    refact: events structure and style

--- src/containers/Events/EventDetails.ts
+++ src/containers/Events/EventDetails.ts
@@ -1,24 +1,18 @@
 import styled, { css } from 'styled-components'
 
 export const Wrapper = styled.div`
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  background-color: #f6f7f8;
+`
+export const DesktopContainer = styled.div`
+  max-width: ${({ theme }) => theme.containers.desktop};
+  width: 100%;
+  margin: 0 auto;
 `
 
 export const HeaderContainer = styled.div`
   width: 100%;
-  height: 19rem;
-  background-color: #fff;
-  border: 1px solid ${({ theme }) => theme.colors.lighterGray};
-  color: #000;
-
-  padding: 2.4rem 2rem;
-`
-
-export const HeaderContent = styled.div`
-  width: 112rem; //1120px
-  height: 100%;
-  margin: 0 auto;
+  padding-top: 2rem;
 `
 
 export const DateHeader = styled.p`
@@ -30,119 +24,170 @@ export const EventTitle = styled.h1`
   ${({ theme }) => css`
     font-weight: ${theme.font.weight.bold};
     font-size: ${theme.font.sizes.xlarge};
-    margin-top: 0.3rem;
-  `}
-`
-export const HostHeader = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 1.5rem;
-  margin-top: 1.5rem;
-`
-export const FakePersonIcon = styled.div`
-  width: 5rem;
-  height: 5rem;
-  border-radius: 50%;
-  background-color: #ccc;
-`
-export const HostedBy = styled.div`
-  display: flex;
-  align-items: flex-start;
-  justify-content: center;
-  flex-direction: column;
-  gap: 0.3rem;
-`
-
-export const HostName = styled.span`
-  ${({ theme }) => css`
-    font-weight: ${theme.font.weight.semiBold};
+    padding-top: 0.3rem;
+    padding-bottom: 1rem;
   `}
 `
 
 export const MainInfoContainer = styled.div`
   width: 100%;
-  height: 100%;
-  background-color: #f6f7f8;
-  border: 1px solid ${({ theme }) => theme.colors.lightGray};
-  padding: 2.4rem 2rem;
-`
-
-export const EventInfoContainer = styled.div`
-  width: 112rem; //1120px
-  height: 100%;
-  margin: 0 auto;
 
   display: grid;
   grid-template-columns: 66rem 46rem;
 `
 
-export const EventInfo = styled.div``
+export const EventInfoContainer = styled.div``
+export const EventImgContainer = styled.div`
+  width: 100%;
+  max-height: 37rem;
+  margin-bottom: 2.2rem;
 
-export const Details = styled.div``
-
-export const H2 = styled.h2`
-  margin-bottom: 1rem;
+  img {
+    border-radius: 1rem;
+  }
 `
-export const PInfo = styled.p`
+
+export const EventDetailsContainer = styled.ul`
+  list-style: none;
+  margin-bottom: 0.7rem;
+`
+export const SubTitle = styled.h3`
+  padding-bottom: 2rem;
+`
+export const EventDetailsItem = styled.li`
+  padding-bottom: 1.5rem;
+`
+export const EventDetailsIcon = styled.span`
+  padding-right: 1rem;
+`
+
+export const DescriptionContainer = styled.div`
+  margin-bottom: 2rem;
+`
+
+export const PDetails = styled.p`
   text-align: justify;
-  margin-bottom: 1rem;
+
+  & + p {
+    margin-bottom: 1.5rem;
+  }
 `
 
-export const SideInfo = styled.div``
-
-export const ScheduleContainer = styled.div`
-  width: 35rem;
-  margin: 0 auto;
-  padding: 2rem;
-  border-radius: 1.5rem;
-  background-color: ${({ theme }) => theme.colors.white};
-`
-
-export const ScheduleContent = styled.div`
+export const SideInfo = styled.div`
+  width: 100%;
+  height: 100%;
   display: flex;
   align-items: center;
-  gap: 1.5rem;
+  flex-direction: column;
+`
 
+export const SideCardContainer = styled.div`
+  width: 35rem;
+
+  padding: 2.5rem;
+  border-radius: 2.5rem;
+  background-color: ${({ theme }) => theme.colors.white};
+  box-shadow: 0px 0px 150px rgba(0, 0, 0, 0.3);
+`
+export const Hoste = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1.8rem;
   margin-bottom: 1.5rem;
 `
+export const ImgHoste = styled.div``
+export const NameAndMedia = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-end;
+  flex-direction: column;
+`
+export const NameHoste = styled.p`
+  margin-bottom: 0.8rem;
+`
+export const MediaHoste = styled.p`
+  ${({ theme }) => css`
+    color: ${theme.colors.darkGray};
+    font-size: ${theme.font.sizes.xsmall};
+  `}
+`
+export const AboutHoste = styled.p``
 
-export const ScheduleInfo = styled.p`
-  width: 20rem;
+export const FloatingInfo = styled.div`
+  ${({ theme }) => css`
+    width: 100%;
+    background-color: ${theme.colors.white};
+    border-radius: 1.5rem 1.5rem 0 0;
+  `}
 `
 
-export const IconContainer = styled.div`
-  width: 3rem;
-  height: 3rem;
-  border-radius: 50%;
-  background-color: #ccc;
+export const ScheduleContainer = styled.div`
+  ${({ theme }) => css`
+    max-width: ${theme.containers.desktop};
+    margin: 0 auto;
+    width: 100%;
+
+    padding: 2rem 0;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  `}
 `
 
-export const FloatingInfo = styled.div``
-
-export const FooterContainer = styled.footer`
-  background-color: #212121;
-
-  width: 100%;
-  height: 8rem;
-  border: 1px solid #fff;
-`
-export const FooterContent = styled.div`
-  width: 112rem;
-  margin: 0 auto;
-
+export const DateTimeContainer = styled.div`
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  gap: 3rem;
+
+  width: fit-content;
 `
 
-export const SocialMedia = styled.div``
-export const SocialIcons = styled.div`
+export const DateTime = styled.div`
+  width: fit-content;
   display: flex;
-  align-items: center;
-  gap: 1.5rem;
+  align-items: flex-start;
+  justify-content: flex-start;
+  flex-direction: column;
+  gap: 1rem;
 `
-export const MobileLinks = styled.div`
+
+export const DateTimeTitle = styled.p`
+  ${({ theme }) => css`
+    color: ${theme.colors.primary};
+    font-weight: ${theme.font.weight.semiBold};
+  `}
+`
+export const DateTimeInfo = styled.strong`
+  font-size: ${({ theme }) => theme.font.sizes.large};
+`
+export const AttendEventContainer = styled.div`
+  width: fit-content;
   display: flex;
   align-items: center;
-  gap: 1.5rem;
+  gap: 4rem;
+`
+export const Price = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  flex-direction: column;
+  gap: 0.8rem;
+`
+export const PriceTitle = styled.span`
+  ${({ theme }) => css`
+    color: ${theme.colors.darkGray};
+    font-size: ${theme.font.sizes.xsmall};
+  `}
+`
+export const PriceValue = styled.span`
+  ${({ theme }) => css`
+    color: ${theme.colors.primary};
+    font-size: ${theme.font.sizes.large};
+  `}
+`
+
+export const SaveEvent = styled.button`
+  background: transparent;
+  outline: none;
+  border: none;
 `
--- src/containers/Events/[id].tsx
+++ src/containers/Events/[id].tsx
@@ -1,92 +1,125 @@
+import axios from 'axios'
+import BookMark from 'components/BookMark'
+import Button from 'components/Button'
+import MeditationIcon from 'components/MeditationIcon'
 import { GetServerSideProps } from 'next'
 import Image from 'next/image'
-import { withSSRAuth } from 'utils/withSSRAuth'
+import { EventData } from 'types/types'
 import * as S from './EventDetails'
 
-export default function EventsDetails() {
+type EventsDetailsProps = {
+  eventData: EventData
+}
+
+export default function EventsDetails({ eventData }: EventsDetailsProps) {
+  const { description, date, name, isOnline, users } = eventData
+
   return (
     <S.Wrapper>
-      <S.HeaderContainer>
-        <S.HeaderContent>
-          <S.DateHeader>Saturday, April 30, 2022</S.DateHeader>
-          <S.EventTitle>
-            [Online] Find a Friend in New Yourk - Networking event
-          </S.EventTitle>
-          <S.HostHeader>
-            <S.FakePersonIcon />
-            <S.HostedBy>
-              Hosted By <S.HostName>Nancy</S.HostName>
-            </S.HostedBy>
-          </S.HostHeader>
-        </S.HeaderContent>
-      </S.HeaderContainer>
+      <S.DesktopContainer>
+        <S.HeaderContainer>
+          <S.DateHeader>{date}</S.DateHeader>
+          <S.EventTitle>{`${isOnline && '[Online]'} ${name}`}</S.EventTitle>
+        </S.HeaderContainer>
 
-      <S.MainInfoContainer>
-        <S.EventInfoContainer>
-          <S.EventInfo>
-            <Image
-              width={620}
-              height={380}
-              src="/eventFake.webp"
-              alt="Default Img"
-            />
-            <S.Details>
-              <S.H2>Details</S.H2>
-              <S.PInfo>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Molestias aut, repellat ipsum facere voluptate dicta obcaecati
-                deserunt nobis suscipit eaque?
-              </S.PInfo>
-              <S.PInfo>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Molestias aut, repellat ipsum facere voluptate dicta obcaecati
-                deserunt nobis suscipit eaque?
-              </S.PInfo>
-            </S.Details>
-          </S.EventInfo>
+        <S.MainInfoContainer>
+          <S.EventInfoContainer>
+            <S.EventImgContainer>
+              <Image
+                width={660}
+                height={370}
+                src="/img/eventFake.png"
+                alt="Default Img"
+              />
+            </S.EventImgContainer>
+
+            <S.EventDetailsContainer>
+              <S.SubTitle>Esse evento inclui: </S.SubTitle>
+              <S.EventDetailsItem>
+                <S.EventDetailsIcon>👍🏻 </S.EventDetailsIcon>
+                Direct interaction with the instructor
+              </S.EventDetailsItem>
+              <S.EventDetailsItem>
+                <S.EventDetailsIcon>🖥 </S.EventDetailsIcon>
+                Access on mobile and web
+              </S.EventDetailsItem>
+              <S.EventDetailsItem>
+                <S.EventDetailsIcon>🎥 </S.EventDetailsIcon>
+                Session recording after the workshop
+              </S.EventDetailsItem>
+              <S.EventDetailsItem>
+                <S.EventDetailsIcon>⌛️ </S.EventDetailsIcon>1 hour live session
+              </S.EventDetailsItem>
+            </S.EventDetailsContainer>
+
+            <S.DescriptionContainer>
+              <S.SubTitle>Descrição</S.SubTitle>
+              <S.PDetails>{description}</S.PDetails>
+            </S.DescriptionContainer>
+          </S.EventInfoContainer>
+
           <S.SideInfo>
-            <S.ScheduleContainer>
-              <S.ScheduleContent>
-                <S.IconContainer />
-                <S.ScheduleInfo>
-                  Saturday, April 30, 2022 6:00 PM to 7:00 PM BRT Every week on
-                  Saturday
-                </S.ScheduleInfo>
-              </S.ScheduleContent>
-              <S.ScheduleContent>
-                <S.IconContainer />
-                <S.ScheduleInfo>
-                  Online event Link visible for attendees
-                </S.ScheduleInfo>
-              </S.ScheduleContent>
-            </S.ScheduleContainer>
+            <S.SideCardContainer>
+              <S.Hoste>
+                <S.ImgHoste>
+                  <MeditationIcon />
+                </S.ImgHoste>
+                <S.NameAndMedia>
+                  <S.NameHoste>{users.name}</S.NameHoste>
+                  <S.MediaHoste>@{users.name}</S.MediaHoste>
+                </S.NameAndMedia>
+              </S.Hoste>
+              <S.AboutHoste>
+                {users.aboutMe
+                  ? users.aboutMe
+                  : 'Modern Cubist abstract artist, NFT artist, Art educator & bridging the Contemporary with the Digital art realms'}
+              </S.AboutHoste>
+            </S.SideCardContainer>
           </S.SideInfo>
-        </S.EventInfoContainer>
-        <S.FloatingInfo>Teste</S.FloatingInfo>
-      </S.MainInfoContainer>
-      <S.FooterContainer>
-        <S.FooterContent>
-          <S.SocialMedia>
-            <p>Follow us</p>
-            <S.SocialIcons>
-              <S.IconContainer />
-              <S.IconContainer />
-              <S.IconContainer />
-            </S.SocialIcons>
-          </S.SocialMedia>
-          <S.MobileLinks>
-            <S.IconContainer />
-            <S.IconContainer />
-          </S.MobileLinks>
-        </S.FooterContent>
-      </S.FooterContainer>
+        </S.MainInfoContainer>
+      </S.DesktopContainer>
+      <S.FloatingInfo>
+        <S.ScheduleContainer>
+          <S.DateTimeContainer>
+            <S.DateTime>
+              <S.DateHeader>Data</S.DateHeader>
+              <S.DateTimeInfo>25 Oct 2021</S.DateTimeInfo>
+            </S.DateTime>
+            <S.DateTime>
+              <S.DateHeader>Horário</S.DateHeader>
+              <S.DateTimeInfo>19:30</S.DateTimeInfo>
+            </S.DateTime>
+            <S.DateTime>
+              <S.DateHeader>Duração</S.DateHeader>
+              <S.DateTimeInfo>60 min</S.DateTimeInfo>
+            </S.DateTime>
+          </S.DateTimeContainer>
+          <S.AttendEventContainer>
+            <S.Price>
+              <S.PriceTitle>Price</S.PriceTitle>
+              <S.PriceValue>RS: 600</S.PriceValue>
+            </S.Price>
+            <S.SaveEvent>
+              <BookMark />
+            </S.SaveEvent>
+            <Button>Compartilhar</Button>
+            <Button>Participar</Button>
+          </S.AttendEventContainer>
+        </S.ScheduleContainer>
+      </S.FloatingInfo>
     </S.Wrapper>
   )
 }
 
-export const getServerSideProps: GetServerSideProps = withSSRAuth(async () => {
-  //Make the Events info request on server side
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const eventId = ctx.params?.id
+  const response = await axios.get<EventData[]>(
+    `https://thiagosgdev.com/events/list?eventId=${eventId}`
+  )
+
   return {
-    props: {}
+    props: {
+      eventData: response.data[0]
+    }
   }
-})
+}
