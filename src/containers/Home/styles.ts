77bc317e8557961f8eb61c310de3e95be84d3be8
Valdenício Ferreira <valdenicio22@gmail.com>
2022-04-20 01:55:14 -0300

    feat: updated home to new style, selects not finish

--- src/containers/Home/styles.ts
+++ src/containers/Home/styles.ts
@@ -1,19 +1,28 @@
 import styled, { css } from 'styled-components'
 
 export const Wrapper = styled.div`
-  max-width: ${({ theme }) => theme.containers.desktop};
   width: 100%;
-  height: 100%;
-  margin: 0 auto;
+  background-color: ${({ theme }) => theme.colors.white};
 `
 
-export const HeaderContainer = styled.header`
-  width: 100%;
+export const HomeContainer = styled.div`
+  ${({ theme }) => css`
+    max-width: ${theme.containers.desktop};
+    width: 100%;
+    margin: 0 auto;
+  `}
+`
+
+export const FiltersContainer = styled.div`
+  margin-top: 3rem;
   display: flex;
-  align-items: flex-start;
-  justify-content: center;
-  flex-flow: column nowrap;
-  margin-bottom: 3rem;
+  align-items: center;
+  gap: 1.5rem;
+
+  select {
+    width: 270px;
+    height: 4.5rem;
+  }
 `
 
 export const MainLinksContainer = styled.div`
@@ -34,18 +43,6 @@ export const MainLinksContainer = styled.div`
   }
 `
 
-export const FiltersContainer = styled.div`
-  margin: 2.5rem 0;
-  display: flex;
-  align-items: center;
-  gap: 1.5rem;
-
-  button {
-    border-radius: 2rem;
-    width: 15rem;
-  }
-`
-
 export const WelcomeContainer = styled.div`
   display: flex;
   align-items: center;
@@ -57,11 +54,29 @@ export const Welcome = styled.h2`
   `}
 `
 
-export const EventsCardsListContainer = styled.main`
+export const Title = styled.h2`
+  ${({ theme }) => css`
+    color: ${theme.colors.secondary};
+    font-weight: ${theme.font.weight.bold};
+    font-size: ${theme.font.sizes.xlarge};
+    line-height: 3rem;
+    padding: 3rem 0;
+  `}
+`
+export const Boosted = styled.div`
   width: 100%;
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(33rem, 1fr));
-  gap: 0 2.5rem;
+  grid-template-columns: minmax(36rem, 1fr);
+  gap: 0 2rem;
+  a {
+    text-decoration: none;
+  }
+`
+export const Recommended = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(3, minmax(36rem, 1fr));
+  gap: 2rem 2.5rem;
   a {
     text-decoration: none;
   }
