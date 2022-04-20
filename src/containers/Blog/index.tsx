5fa8f0c678e0f10bf3054d72d41c2d83f8ef4e55
Valdenício Ferreira <valdenicio22@gmail.com>
2022-04-20 14:35:49 -0300

    feat: create and style the Blog page

--- src/containers/Blog/index.tsx
+++ src/containers/Blog/index.tsx
@@ -1,7 +1,24 @@
 import * as S from './styles'
 
 const Blog = () => {
-  return <S.Wrapper>Dicas de Saúde</S.Wrapper>
+  return (
+    <S.Wrapper>
+      <S.BlogContainer>
+        <S.Welcome>Dicas de bem-estar</S.Welcome>
+        <S.BlogCard>
+          <S.ImgContainer>
+            <img src="/img/blog.png" alt="Descrição da imagem" />
+          </S.ImgContainer>
+          <S.InfoContainer>
+            <S.CardTitle>Chás para dormir melhor</S.CardTitle>
+            <S.CardDescription>
+              Iniciando o ritual para uma boa noite{' '}
+            </S.CardDescription>
+          </S.InfoContainer>
+        </S.BlogCard>
+      </S.BlogContainer>
+    </S.Wrapper>
+  )
 }
 
 export default Blog
--- src/containers/Blog/styles.tsx
+++ src/containers/Blog/styles.tsx
@@ -1,3 +1,50 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-export const Wrapper = styled.h1``
+export const Wrapper = styled.div`
+  ${({ theme }) => css`
+    width: 100%;
+    background-color: ${theme.colors.white};
+  `}
+`
+
+export const BlogContainer = styled.div`
+  ${({ theme }) => css`
+    max-width: ${theme.containers.desktop};
+    margin 0 auto;
+  `}
+`
+export const Welcome = styled.h1`
+  ${({ theme }) => css`
+    color: ${theme.colors.secondary};
+    font-size: ${theme.font.sizes.xxlarge};
+    font-weight: ${theme.font.weight.bold};
+    padding: 2.5rem 0;
+  `}
+`
+export const BlogCard = styled.div`
+  width: 36rem;
+  height: 40.8rem;
+
+  ${({ theme }) => css`
+    background-color: ${theme.colors.mediumGray};
+    border-radius: ${theme.border.radius.large};
+  `}
+`
+
+export const ImgContainer = styled.div`
+  width: 100%;
+  height: 30.05rem;
+`
+export const InfoContainer = styled.div`
+  width: 100%;
+  padding: 1.5rem 2.5rem;
+  color: ${({ theme }) => theme.colors.black};
+`
+export const CardTitle = styled.p`
+  font-size: ${({ theme }) => theme.font.sizes.xlarge};
+  font-weight: ${({ theme }) => theme.font.weight.bold};
+  padding-bottom: 1.2rem;
+`
+export const CardDescription = styled.p`
+  font-size: ${({ theme }) => theme.font.sizes.large};
+`
