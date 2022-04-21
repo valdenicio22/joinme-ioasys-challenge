e51eff4f66b4814eee3d1e2cabdea3f3e9240549
Valdenício Ferreira <valdenicio22@gmail.com>
2022-04-21 01:44:12 -0300

    feat: add logic and style to MyEvents page

--- src/containers/MyEvents/style.ts
+++ src/containers/MyEvents/style.ts
@@ -1,3 +1,30 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-export const Wrapper = styled.div``
+export const Wrapper = styled.div`
+  ${({ theme }) => css`
+    width: 100%;
+    background-color: ${theme.colors.white};
+  `}
+`
+
+export const MyEventsContainer = styled.div`
+  ${({ theme }) => css`
+    max-width: ${theme.containers.desktop};
+    margin 0 auto;
+  `}
+`
+export const Welcome = styled.h1`
+  ${({ theme }) => css`
+    color: ${theme.colors.secondary};
+    font-size: ${theme.font.sizes.xxlarge};
+    font-weight: ${theme.font.weight.bold};
+    padding: 2.5rem 0;
+  `}
+`
+
+export const MyCardList = styled.main`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(36rem, 1fr));
+  gap: 2rem;
+  padding-bottom: 3rem;
+`
