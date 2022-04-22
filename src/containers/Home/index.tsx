175b14bfe4e6d69b5fcdcede957d696fff0eb598
Valdenício Ferreira <valdenicio22@gmail.com>
2022-04-22 14:28:24 -0300

    fix: bring back ssr interests

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -9,16 +9,16 @@ import { useEvents } from 'hooks/useEvents'
 import { CurrentModal, Activity } from 'types/types'
 
 import { UserDialog } from 'components/UserDialog'
-//import { GetServerSideProps } from 'next'
-//import axios from 'axios'
-//import { parseCookies } from 'nookies'
+import { GetServerSideProps } from 'next'
+import axios from 'axios'
+import { parseCookies } from 'nookies'
 import { useAuth } from '../../context/AuthContext'
 
 type HomeProps = {
   userInterests: Array<Activity>
 }
 
-export default function Home({ userInterests = [] }: HomeProps) {
+export default function Home({ userInterests }: HomeProps) {
   const { user } = useAuth()
   const { events } = useEvents()
   const [currentModal, setCurrentModal] = useState<CurrentModal>(
@@ -106,7 +106,6 @@ export default function Home({ userInterests = [] }: HomeProps) {
   )
 }
 
-/*
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const cookies = parseCookies(ctx)
   if (cookies.joinMeToken) {
@@ -132,4 +131,3 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
     }
   }
 }
-*/
