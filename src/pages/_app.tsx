4b590d5f4de964ab7cd14d8496bfccf4886c9da5
Valdenício Ferreira <valdenicio22@gmail.com>
2022-04-22 10:10:11 -0300

    feat: remove pwa

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,12 +16,8 @@ function App({ Component, pageProps }: AppProps) {
       <Head>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#06092B" />
-        <meta
-          name="description"
-          content="A project that helps people with mental health issues"
-        />
+
+        <meta name="description" content="A project that helps people" />
       </Head>
       <GlobalStyles />
       <AuthProvider>
